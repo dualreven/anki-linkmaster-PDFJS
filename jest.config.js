e921960ecdcedebd8b05ac2c24b1463e257ae981
黄集攀 <lifeiyu1992@qq.com>
2025-09-15 16:57:44 +0800

    完善前端消息队列机制，改进Jest配置支持ES模块

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  // 在测试环境创建前注入 polyfill（如 fake-indexeddb）
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(tabulator-tables|pdfjs-dist)/)',
-  ],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  }
+  setupFiles: ['<rootDir>/jest.setup.js']
 };
